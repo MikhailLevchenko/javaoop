727c7cb69c380cf6d23742bd1a815c49de2c852b
MikhailLevchenko <33897268+MikhailLevchenko@users.noreply.github.com>
2018-01-22 00:03:01 +0200

    Add files via upload

---
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/GroupInsertionException.java
@@ -0,0 +1,34 @@
+package com.yahoo.mikhailjs.javaoop.group;
+
+public class GroupInsertionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2073977529850697863L;
+
+	public GroupInsertionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupInsertionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupInsertionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupInsertionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupInsertionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/GroupOverflowException.java
@@ -0,0 +1,35 @@
+package com.yahoo.mikhailjs.javaoop.group;
+
+public class GroupOverflowException extends GroupInsertionException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4556123574117512894L;
+
+	public GroupOverflowException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupOverflowException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupOverflowException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupOverflowException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GroupOverflowException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
