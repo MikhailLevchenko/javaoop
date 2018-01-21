60c9f057e4fd486f587b812b48588e5e402efab3
MikhailLevchenko <33897268+MikhailLevchenko@users.noreply.github.com>
2018-01-22 00:41:45 +0200

    Add files via upload

--- GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Content.java
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Content.java
@@ -6,26 +6,23 @@ import java.util.Random;
 
 public class Content {
 
-	private static String[] firstNamesMale = new String[] {"Сергей",
-			"Владимир", "Антон", "Игорь", "Борис", "Алексей",
-			"Григорий", "Юрий", "Олег", "Ярослав","Мстислав",
-			"Денис", "Виктор", "Захар", "Арсен", "Тарас", "Назар",
-			"Виталий", "Степан", "Ростислав"};
-	private static String[] lastNamesMale = new String[] {"Кузнецов",
-			"Смирнов", "Мельник", "Бойко", "Шевчук", "Неделин",
-			"Цыбин", "Захарчук", "Дашков", "Терюшин", "Володин",
-			"Олейник", "Исаев", "Афонов", "Остапчук", "Хорунжий",
-			"Устимов", "Пермин", "Репьев", "Журов"};
-	private static String[] firstNames = new String[] {"Инна",
-			"Лариса", "Ольга", "Ксения", "Полина", "Светлана",
-			"Мария", "Ирина", "София", "Людмила", "Оксана", "Олеся",
-			"Виктория", "Галина", "Вероника", "Лина", "Дарья",
-			"Алиса", "Марина", "Карина"};
-	private static String[] lastNames = new String[] {"Кузнецова",
-			"Смирнова", "Мельник", "Бойко", "Шевчук", "Неделина",
-			"Цыбина", "Захарчук", "Дашкова", "Терюшина", "Володина",
-			"Олейник", "Исаева", "Афонова", "Остапчук", "Хорунжая",
-			"Устимова", "Перминова", "Репьева", "Журова"};
+	private static String[] firstNamesMale = new String[] {"Сергей", "Антон",
+			"Владимир", "Игорь", "Борис", "Алексей", "Юрий", "Тарас", "Олег",
+			"Ярослав", "Денис", "Степан", "Виктор", "Захар", "Арсен", "Назар",
+			"Виталий","Мстислав", "Ростислав", "Григорий"};
+	private static String[] lastNamesMale = new String[] {"Володин", "Исаев",
+			"Смирнов", "Мельник", "Бойко", "Шевчук", "Неделин","Цыбин",
+			"Захарчук", "Дашков", "Терюшин", "Кузнецов","Олейник", "Афонов",
+			"Остапчук", "Хорунжий", "Устимов", "Пермин", "Репьев", "Журов"};
+	private static String[] firstNames = new String[] {"Инна", "Лина", "Ольга",
+			"Ксения", "Полина", "Дарья", "Мария", "Ирина", "София","Вероника",
+			"Оксана", "Олеся", "Галина", "Карина", "Алиса", "Лариса", "Марина",
+			"Виктория", "Светлана", "Людмила"};
+	private static String[] lastNames = new String[] { "Володина", "Исаева",
+			"Смирнова", "Мельник", "Бойко", "Шевчук", "Неделина", "Цыбина",
+			"Захарчук", "Дашкова", "Терюшина","Кузнецова", "Олейник",
+			"Афонова", "Остапчук", "Хорунжая", "Устимова", "Перминова",
+			"Репьева", "Журова"};
 	private static Random randoms = new Random();
 	
 	public static String randomFristName(boolean sex) {
@@ -43,7 +40,8 @@ public class Content {
 		String lastName;
 
 		if(sex) {
-			lastName = lastNamesMale[randoms.nextInt(lastNamesMale.length)];
+			lastName = lastNamesMale[randoms.nextInt(
+									 lastNamesMale.length)];
 		} else {
 			lastName = lastNames[randoms.nextInt(lastNames.length)];
 		}
@@ -54,18 +52,19 @@ public class Content {
 
 		boolean sex = randoms.nextBoolean();
 		String firstName = randomFristName(sex);
-		String laststName = randomLastName(sex);
+		String lastName = randomLastName(sex);
 		Calendar naw = Calendar.getInstance();
 		int age = (int) (16+Math.random()*30);
 		Date birthDate;
 		
 		naw.add(Calendar.YEAR, -age);
-		naw.set(Calendar.DAY_OF_YEAR, randoms.nextInt(naw.getActualMaximum(Calendar.DAY_OF_YEAR)));
+		naw.set(Calendar.DAY_OF_YEAR, randoms.nextInt(
+				naw.getActualMaximum(Calendar.DAY_OF_YEAR)));
 		naw.set(Calendar.MILLISECOND, 0);
 		naw.set(Calendar.SECOND, 0);
 		naw.set(Calendar.MINUTE, 0);
 		naw.set(Calendar.HOUR_OF_DAY, 0);
 		birthDate = naw.getTime();
-		return new Human(firstName, laststName, birthDate, sex);
+		return new Human(firstName, lastName, birthDate, sex);
 	}
 }
