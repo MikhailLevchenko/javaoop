8dee3e6bccffe0b92fc66115bdb261bac5038c8c
MikhailLevchenko <33897268+MikhailLevchenko@users.noreply.github.com>
2018-01-22 01:32:06 +0200

    Update Human.java

--- GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Human.java
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Human.java
@@ -5,7 +5,6 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Human {
-
 	private String firstName;
 	private String lastName;
 	private Date birthDate;
@@ -23,18 +22,16 @@ public class Human {
 	}
 
 	public int getAge() {
-		
 		Calendar current = Calendar.getInstance();
 		Calendar birth = Calendar.getInstance();
+		int years;
 		
 		birth.setTime(birthDate);
-		
-		int years = current.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
-		
-		if(current.get(Calendar.MONTH) < birth.get(Calendar.MONTH)) {
+		years = current.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
+		if (current.get(Calendar.MONTH) < birth.get(Calendar.MONTH)) {
 			years--;
-		} else if(current.get(Calendar.MONTH) == birth.get(Calendar.MONTH)
-				&& current.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH)) {
+		} else if (current.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH)
+				&& current.get(Calendar.MONTH) == birth.get(Calendar.MONTH)) {
 			years--;
 		}
 		return years;
@@ -58,7 +55,6 @@ public class Human {
 
 	@Override
 	public String toString() {
-		
 		SimpleDateFormat fmt = new SimpleDateFormat("dd MMMM y");
 		
 		return String.format("%s %s %s", firstName, lastName, fmt.format(birthDate));
