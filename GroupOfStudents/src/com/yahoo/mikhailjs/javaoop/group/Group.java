b04cf0ecdc8ba785f50969802ba8cd1586d6d366
MikhailLevchenko <33897268+MikhailLevchenko@users.noreply.github.com>
2018-01-23 16:48:22 +0200

    Add files via upload

---
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Group.java
@@ -0,0 +1,180 @@
+package com.yahoo.mikhailjs.javaoop.group;
+
+import java.util.Arrays;
+
+public class Group {
+	private Student[] members;
+
+	public Group(int maxMembers, Student[] people) {
+		this(maxMembers);
+		try {
+			addStudents(people);
+		} catch (GroupInsertionException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public Group(int maxMembres) {
+		super();
+		members = new Student[maxMembres];
+	}
+
+	public Group() {
+		this(10);
+	}
+
+	public Student[] getMembers() {
+		return Arrays.copyOf(members, members.length);
+	}
+
+	/**
+	 * 
+	 * @param students
+	 * @throws GroupInsertExeption
+	 */
+	public void setMembers(Student[] students) 
+			throws GroupInsertionException {
+		addStudents(students);
+	}
+
+	/**
+	 * 
+	 * @param students
+	 * @throws GroupInsertExeption
+	 */
+	private void addStudents(Student[] students) 
+			throws GroupInsertionException {
+		for (Object i : students) {
+			try {
+				if (i instanceof Student) {
+					addStudent((Student) i);
+				}
+			} catch (GroupOverflowException e) {
+				break;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param student
+	 * @throws GroupInsertExeption
+	 * @throws GroupOverflowException
+	 */
+	public Student addStudent(Student student) throws GroupInsertionException {
+
+		String lastName = student.getLastName();
+
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] != null) {
+				continue;
+			} else if (isInGroup(lastName)) {
+				throw new GroupInsertionException(String.format("Студент"
+							+ " с фамилией %s уже в гуппе.", lastName));
+			} else {
+				members[i] = student;
+				return members[i];
+			}
+		}
+		throw new GroupOverflowException("Group overflowing.");
+	}
+
+	private boolean isInGroup(String lastName) {
+		return (getStudByLastName(lastName) != null);
+	}
+	
+	public Student removeByLastName(String lastName) {
+		Student student = null;
+		
+		for(int i = 0; i < members.length; i++) {
+			if (members[i] == null) {
+				continue;
+			} else if (members[i].getLastName().equals(lastName)) {
+				student = members[i];
+				members[i] = null;
+				break;
+			}
+		}
+		return student;
+	}
+
+	public Student getStudByLastName(String lastName) {
+		Student student = null;
+
+		for (Student i : members) {
+			if (i == null) {
+				continue;
+			} else if (i.getLastName().equals(lastName)) {
+				student = i;
+			}
+		}
+		return student;
+	}
+
+	public boolean isEmpty() {
+		boolean empty = true;
+		
+		for(Student i : members) {
+			if(i != null) {
+				empty = false;
+				break;
+			}
+		}
+		return empty;
+	}
+
+	@Override
+	public String toString() {
+		String sep = System.lineSeparator();
+		StringBuilder sb = new StringBuilder();
+		int counter = 0;
+
+		for (Student i = getFirstStud(); i != null; i = getNextStud(i)) {
+			counter++;
+			sb.append(String.format("%d). %s%s", counter, i, sep));
+		}
+		return sb.toString();
+	}
+
+	private Student getFirstStud() {
+		Student stud = null;
+
+		for (Student i : members) {
+			if (i == null) {
+				continue;
+			}
+			if (stud == null) {
+				stud = i;
+			} else if (stud.getLastName().compareTo(i.getLastName()) > 0) {
+				stud = i;
+			}
+		}
+		return stud;
+	}
+
+	private Student getNextStud(Student stud) {
+		Student next = null;
+
+		for (Student i : members) {
+			if (i == null) {
+				continue;
+			}
+			if (next == null) {
+				if (stud.getLastName().compareTo(i.getLastName()) < 0) {
+					next = i;
+				}
+			} else {
+				if ((stud.getLastName().compareTo(i.getLastName()) < 0)) {
+					if (next.getLastName().compareTo(i.getLastName()) > 0) {
+						next = i;
+					}
+				}
+			}
+		}
+		return next;
+	}
+	
+	public int getMaxMembers() {
+		return members.length;
+	}
+}
---
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Main.java
@@ -0,0 +1,101 @@
+package com.yahoo.mikhailjs.javaoop.group;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Group studGroup = new Group();
+
+		for (;;) {
+			try {
+				addRandomStud(studGroup);
+			} catch (GroupOverflowException e) {
+				break;
+			}
+		}
+
+		System.out.println(studGroup);
+
+		Scanner sc = new Scanner(System.in);
+		String s = null;
+
+		for (;;) {
+			s = dlgActions(sc);
+			if (s == null || s.length() < 1) {
+				continue;
+			}
+			if (s.charAt(0) == 'Q') {
+				break;
+			}
+			switch (s.charAt(0)) {
+			case 'W': {
+				printGroupRows(studGroup);
+				break;
+			}
+			case 'R': {
+				System.out.println(" Remove fom group by last name:");
+				studGroup.removeByLastName(sc.nextLine());
+				break;
+			}
+			case 'P': {
+				printGroup(studGroup);
+				break;
+			}
+			case 'A': {
+				try {
+					addRandomStud(studGroup);
+				} catch (GroupOverflowException e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			}
+			}
+		}
+		sc.close();
+		System.out.println("Quit Program.");
+
+	}
+
+	public static String dlgActions(Scanner sc) {
+		System.out.println("P print, W print rows, R remove,"
+	+ " A add random, Q exit:");
+		return sc.nextLine();
+	}
+
+	public static void printGroup(Group group) {
+		System.out.println(group);
+	}
+
+	public static void printGroupRows(Group group) {
+		StringBuilder sb = new StringBuilder();
+		int counter = 0;
+
+		for (Student i : group.getMembers()) {
+			counter++;
+			if (i == null) {
+				sb.append(String.format("%d). [ empty ]", counter));
+				sb.append(System.lineSeparator());
+				continue;
+			}
+			sb.append(String.format("%d). [ %s %s ]", counter,
+					i.getFirstName(), i.getLastName()));
+			sb.append(System.lineSeparator());
+		}
+		System.out.print(sb);
+	}
+
+	public static Student addRandomStud(Group group)
+			throws GroupOverflowException {
+		for (;;) {
+			try {
+				return group.addStudent(new Student(Content.randomHuman(),
+						"Cyber Security"));
+			} catch (GroupOverflowException e) {
+				throw e;
+			} catch (GroupInsertionException e) {
+				continue;
+			}
+		}
+	}
+}
