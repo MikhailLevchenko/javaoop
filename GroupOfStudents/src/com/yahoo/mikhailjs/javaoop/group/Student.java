a4c7c4795be843f78d05d0d037c585c33022ec4f
MikhailLevchenko <33897268+MikhailLevchenko@users.noreply.github.com>
2018-01-23 09:32:32 +0200

    Add files via upload

---
+++ GroupOfStudents/src/com/yahoo/mikhailjs/javaoop/group/Student.java
@@ -0,0 +1,25 @@
+package com.yahoo.mikhailjs.javaoop.group;
+
+public class Student extends Human {
+	String specialty;
+	
+	public Student(Human human, String specialty ) {
+		super(human.getFirstName(), human.getLastName(), human.getBirthDate(),
+				human.isSex());
+		this.specialty = specialty;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s %s study \"%s\"", getFirstName(),
+				getLastName(), getSpecialty());
+	}
+}
